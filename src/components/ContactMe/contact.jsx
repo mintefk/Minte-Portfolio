ff4451659f0bc937997bd1148f8555e5cef3bc7d
mintefk <140588599+mintefk@users.noreply.github.com>
2023-11-30 04:16:55 -0900

    removed email funtion

--- src/components/ContactMe/contact.jsx
+++ src/components/ContactMe/contact.jsx
@@ -1,26 +1,11 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import './contact.css';
 import facebookIcon from "../../assets/facebook-icon.png";
 import TwitterIcon from "../../assets/twitter.png";
 import YouTubeIcon from "../../assets/youtube.png";
 import Linkedin from "../../assets/linkedin.png";
-import emailjs from '@emailjs/browser';
-const Contact = () => {
-  const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
-   
-    emailjs.sendForm('service_zdoxrsw', 'template_mcayjzw', form.current, '3CZSI6q7_iqsCugHu')
-      .then((result) => {
-          console.log(result.text);
-          alert('Email Sent!');
-         
-          form.current.reset();
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
+const Contact = () => {
 
   return (
     <section id='contactpage'>
@@ -29,7 +14,7 @@ const Contact = () => {
                             
      <span className="contactDesc">Please fill out the form below to discuss any work opportunities.</span>
 
-    <form className= 'contactForm' ref={form} onSubmit={sendEmail} >
+    <form className= 'contactForm'  >
         
      <input type="text" className="name" placeholder='Your Name'  name='from_name' required />
      <input type="email" className="email" placeholder='Your Email'  name='email'required/>
@@ -47,6 +32,7 @@ const Contact = () => {
 </section>
 
   )
+  
 }
 
 export default Contact
